be76a23586492d8e2f3512da9a639e7262861bea
Mike Bolaris <mike@bolaris.com>
2018-11-15 18:18:37 -0800

    x

--- public/javascripts/DoorBell.js
+++ public/javascripts/DoorBell.js
@@ -4,11 +4,9 @@
     var bellSound;
     var holidaySound;
     var nextHolidaySound;
-
     var serverPowerLevel = 100;
     var serverAlarmMode = "off";
     var holidayMode = "off";
-    
     var soundIndex = 0;
     
 	function buttonPress() {
@@ -31,31 +29,25 @@
 	}
 
 	function alarmModeSwitch(sel) {
-		
 		if (sel.value != serverAlarmMode) {
 			socket.emit('alarmMode', sel.value);	
 		}	
 	}
 	
 	function powerLevel(value) {
-		
 		if (value != serverPowerLevel) {
 			socket.emit('powerLevel', value);	
 		}		
 	}
 
 	window.onload = function() {
-
 		socket.on('ringStart', function(data) {
 			ringCount.innerHTML = 'Ring Count: ' + data;
-
-			if (soundEnabled) {
-					
+			if (soundEnabled) {					
 				if (holidayMode == "on") {
 			     holidaySound = nextHolidaySound;
 				holidaySound.play();
-				}
-				else {
+				} else {
 					bellSound.play();
 				}							
 			}
@@ -65,8 +57,7 @@
                if (holidayMode == "on") {
                     holidaySound = nextHolidaySound;
                     holidaySound.play();
-               }
-               else {
+               } else {
                     bellSound.play();
                }
           });
@@ -79,32 +70,25 @@
 		});
 
 		socket.on('alarmMode', function(data) {
-			
 			serverAlarmMode = data;
-			
 			if ($("#alarmMode").value != serverAlarmMode) {
  		    	$("#alarmMode").val(serverAlarmMode).slider("refresh");
 			}
 		});
 
 		socket.on('holidayMode', function(data) {
-			
 			holidayMode = data;
-			
 			if ($("#holidayMode").value != holidayMode) {
  		    	$("#holidayMode").val(holidayMode).slider("refresh");
 			}
 		});
 		
 	        socket.on('newHolidaySound', function(data) {
-	             
 	             nextHolidaySound = new Audio(data);
 	          });
 		
 		socket.on('powerLevel', function(data) {
-			
 			serverPowerLevel = data;
-			
 			if ($("#powerSlider").value != serverPowerLevel) {
 				$("#powerSlider").val(serverPowerLevel).slider("refresh");
 			}		
@@ -113,12 +97,10 @@
 		socket.on('bellBlogUpdate', function(data) {
 			var table = document.getElementById("bellBlogTable");
 			var row = table.insertRow(0);
-			
 			var cell1 = row.insertCell(0);
 			cell1.innerHTML = data.timestamp;
 			var cell2 = row.insertCell(1);
 			cell2.innerHTML = data.message;
-
 			if (table.rows.length > 50) {
 				table.deleteRow(-1);
 			}
