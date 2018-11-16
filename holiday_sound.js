517dc4434be236b774f7e28adcdb3f6589fedc7e
Mike Bolaris <mike@bolaris.com>
2018-11-15 20:13:48 -0800

    x

--- holiday_sound.js
+++ holiday_sound.js
@@ -1,12 +1,6 @@
 
 
-var sounds=["sounds/halloween/Dark_Laugh-HopeinAwe-1491150192.wav",
-            "sounds/halloween/Demon_Girls_Mockingbir-Hello-1365708396.wav",
-            "sounds/halloween/Elk Bellowing In Forest-SoundBible.com-681308174.wav",
-            "sounds/halloween/Monster_Gigante-Doberman-1334685792.wav",
-            "sounds/halloween/Mummy Zombie-SoundBible.com-1966938763.wav",
-            "sounds/halloween/ooow.wav",
-		    "sounds/halloween/ghostly1.wav", 
+var sounds=["sounds/halloween/ghostly1.wav", 
             "sounds/halloween/ghostly2.wav", 
             "sounds/halloween/happyhw.wav", 
             "sounds/halloween/hauntmanwelcome.wav", 
