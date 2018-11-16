c4adc34a155298632e968c1ee6a25bd5ba147859
Mike Bolaris <mike@bolaris.com>
2018-11-15 18:09:31 -0800

    x

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,14 @@ var bellcontroller = require('../doorbell-controller');
 var bellSettings = require('../doorbell-settings');
 var holidaySound = require('../holiday_sound');
 var bellHistory = require('../doorbell-history');
-
-
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
 	console.log('powerLevel = ' + bellSettings.powerLevel);
 	console.log('currentAlarmMode = ' + bellSettings.currentAlarmMode);	
-
-	//
 	// Find items logged between today and yesterday.
-	//
 	bellHistory.getRecentLog(function(results) {
-       
-      console.log(results);
-
       res.render('doorbell', {
 			powerLevel : bellSettings.powerLevel,
 			currentAlarmMode : bellSettings.currentAlarmMode,
