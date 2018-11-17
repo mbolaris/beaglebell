bc98aab40fabd275e2aa2f891b34beb1a2934b37
Mike Bolaris <mike@bolaris.com>
2018-11-16 18:51:04 -0800

    x

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@ var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log('motion detected');
+	console.log('powerLevel = ' + bellSettings.powerLevel);
+	console.log('currentAlarmMode = ' + bellSettings.currentAlarmMode);	
+	// Find items logged between today and yesterday.
+	bellHistory.getRecentLog(function(results) {
+      res.render('doorbell', {
+			powerLevel : bellSettings.powerLevel,
+			currentAlarmMode : bellSettings.currentAlarmMode,
+			currentHolidayMode : bellSettings.currentHolidayMode,
+			ringCount : bellHistory.ringCount,
+			logging : results.file,
+			nextHolidaySound : holidaySound.getNextSound()
+		});	
+	});
 });
 
 module.exports = router;
--- routes/motion.js
+++ routes/motion.js
@@ -6,19 +6,7 @@ var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log('powerLevel = ' + bellSettings.powerLevel);
-	console.log('currentAlarmMode = ' + bellSettings.currentAlarmMode);	
-	// Find items logged between today and yesterday.
-	bellHistory.getRecentLog(function(results) {
-      res.render('doorbell', {
-			powerLevel : bellSettings.powerLevel,
-			currentAlarmMode : bellSettings.currentAlarmMode,
-			currentHolidayMode : bellSettings.currentHolidayMode,
-			ringCount : bellHistory.ringCount,
-			logging : results.file,
-			nextHolidaySound : holidaySound.getNextSound()
-		});	
-	});
+	console.log('motion detected');
 });
 
 module.exports = router;
