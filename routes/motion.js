a8899e49a8a25bcea1dd90e64156c3fac2915a5f
Mike Bolaris <mike@bolaris.com>
2018-11-17 07:08:37 -0800

    x

--- routes/motion.js
+++ routes/motion.js
@@ -3,6 +3,12 @@ var router = require('express').Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log('motion detected');
+	bellHistory.blog('motion detected');
+	if (bellSettings.currentAlarmMode == "on") {
+		bellHistory.blog('ALARM !!!');
+		bellController.startAlarmPattern();
+	}
+	res.end();
 });
 
 module.exports = router;
